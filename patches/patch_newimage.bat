6d84f62b77fb5465c10a651c43901b1c3d8f39bc
frankyeh <frank.yeh@gmail.com>
2021-12-02 12:07:04 -0500

    Update patch_newimage.bat

--- patches/patch_newimage.bat
+++ patches/patch_newimage.bat
@@ -1,4 +1,3 @@
 powershell -Command "(gc include\NewNifti\nifti2.h) -replace '__attribute__ \(\(__packed__\)\)', '' | Out-File -encoding ASCII include\NewNifti\nifti2.h"
 
-move /y patches\newimage\newimage.cc include\newimage
-move /y patches\newimage\newimage.h include\newimage
+move /y patches\newimage\* include\newimage
