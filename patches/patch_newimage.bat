b8d910f8535e7735cc1a46354fb6844babf64ce6
frankyeh <frank.yeh@gmail.com>
2021-12-03 09:48:53 -0500

    Delete patch_newimage.bat

--- patches/patch_newimage.bat
+++
@@ -1,3 +0,0 @@
-powershell -Command "(gc include\NewNifti\nifti2.h) -replace '__attribute__ \(\(__packed__\)\)', '' | Out-File -encoding ASCII include\NewNifti\nifti2.h"
-
-move /y patches\newimage\* include\newimage
