b669ac348424e1e9dac1a312ce7213ea49f37a7c
frankyeh <frank.yeh@gmail.com>
2021-12-03 09:27:44 -0500

    update patch script

--- patches/patch_FSLOUTPUTTYPE.bat
+++
@@ -1,1 +0,0 @@
-powershell -Command "(gc include\newimage\generalio.cc) -replace 'otype = getenv\(\"FSLOUTPUTTYPE\"\);', 'return FSL_TYPE_NIFTI_GZ;' | Out-File -encoding ASCII include\newimage\generalio.cc"
