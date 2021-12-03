30dd3e422cd405b43109a18ce50d675b86c5fa8e
frankyeh <frank.yeh@gmail.com>
2021-12-03 09:47:10 -0500

    Delete patch_znzlib.bat

--- patches/patch_znzlib.bat
+++
@@ -1,6 +0,0 @@
-(echo #include "zlib.h") > include\znzlib\znzlib.h_
-powershell -Command "(gc include\znzlib\znzlib.h) -replace '#include \"zlib.h\"', '' | Out-File -encoding ASCII include\znzlib\znzlib.h2"
-type include\znzlib\znzlib.h_ include\znzlib\znzlib.h2 > include\znzlib\znzlib.h
-del include\znzlib\znzlib.h2 include\znzlib\znzlib.h_
-
-
