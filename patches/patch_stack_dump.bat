3339045964df808cbfdcd5a3bd7a23656b1f2e94
frankyeh <frank.yeh@gmail.com>
2021-12-03 09:34:09 -0500

    Delete patch_stack_dump.bat

--- patches/patch_stack_dump.bat
+++
@@ -1,6 +0,0 @@
-powershell -Command "(gc include\eddy\eddy.cpp) -replace '#include \"utils/stack_dump.h\"', '' | Out-File -encoding ASCII include\eddy\eddy.cpp"
-powershell -Command "(gc include\eddy\eddy.cpp) -replace 'StackDump::Install\(\);', '' | Out-File -encoding ASCII include\eddy\eddy.cpp"
-
-
-powershell -Command "(gc include\topup\topup.cpp) -replace '#include \"utils/stack_dump.h\"', '' | Out-File -encoding ASCII include\topup\topup.cpp"
-powershell -Command "(gc include\topup\topup.cpp) -replace 'StackDump::Install\(\);', '' | Out-File -encoding ASCII include\topup\topup.cpp"
