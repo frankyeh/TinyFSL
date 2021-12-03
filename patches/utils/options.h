a878247b7b121a9f90f23ec153bb38e4ab3c5cae
frankyeh <frank.yeh@gmail.com>
2021-12-03 10:13:31 -0500

    Update options.h

--- patches/utils/options.h
+++ patches/utils/options.h
@@ -380,11 +380,27 @@ namespace Utilities {
     unsigned int valuevec_size_;
   };
 
-  template<> bool Option<bool>::set_value(const std::string& s);
-  template<> bool Option<bool>::set_value(const std::string& vs, char* argv[], int valpos, int argc);
+  template<> std::string Option<bool>::config_key() const
+  {
+      return set()? (long_form().empty()?short_form():long_form()):std::string();
+  }
+  template<> bool Option<bool>::set_value(const std::string& s)
+  {
+      value_=s.empty()?(!default_):(s=="true"?true:(s=="false"?false:value_));
+      return !(unset_=!s.empty()&&s!="true"&&s!="false");
+  }
+  template<> bool Option<bool>::set_value(const std::string& vs,char*[],int,int)
+  {
+      return set_value(vs);
+  }
+  template<> std::ostream& Option<bool>::print(std::ostream& os) const
+  {
+      os << "# " << help_text() << std::endl;
+      if (set())
+          os << config_key().substr(0, config_key().find("="));
+      return os;
+  }
 
-  template<> std::ostream& Option<bool>::print(std::ostream& s) const;
-  //  std::ostream& operator<<(std::ostream& os, const Option<bool>& o);
   std::ostream& operator<<(std::ostream& os, const BaseOption& o);
 
   template<class T> class HiddenOption: public Option<T>
