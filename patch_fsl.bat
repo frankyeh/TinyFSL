efe9054464e1fe431ea6db37d6cbccd064fceb89
frankyeh <frank.yeh@gmail.com>
2022-02-05 19:57:26 -0500

    Delete patch_fsl.bat

--- patch_fsl.bat
+++
@@ -1,8 +0,0 @@
-cd fsl
-for /f "delims=" %%x in ('dir include\* /b ') do (move /y src\fsl-%%x\* include\%%x) 
-rmdir /S /Q bin
-rmdir /S /Q info
-rmdir /S /Q src
-rmdir /S /Q tcl
-cd ..
-for /f "delims=" %%x in ('dir patches\* /b ') do (copy /y patches\%%x\* fsl\include\%%x) 
